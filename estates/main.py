710e9cba4d5a91173695bb978eafa3a397041d44
suna <sunajp.dev@gmail.com>
2021-05-29 14:47:16 +0900

    init

---
+++ estates/main.py
@@ -0,0 +1,277 @@
+import datetime
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+import hashlib
+import re
+import pandas as pd
+
+from pathlib import Path
+import time
+import os
+
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+
+from estates import get_google_spreadsheet
+
+sumo_domain = "https://suumo.jp/"
+
+# 取得時刻
+NOWTIME = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+
+# 物件情報取得処理
+def get_estate(get_url):
+    r = requests.get(get_url)
+    estates = html_to_estates(r.content)
+    return estates
+
+
+def html_to_estates(html_text):
+    soup = BeautifulSoup(html_text, "html.parser")
+    estates = soup.select("div.property_unit")
+    return estates
+
+
+# 都道府県と市町村を抽出
+def get_prefecture_city(content):
+
+    pattern = "(...??[都道府県])((?:旭川|伊達|石狩|盛岡|奥州|田村|南相馬|那須塩原|東村山|武蔵村山|羽村|十日町|上越|富山|野々市|大町|蒲郡|四日市|姫路|大和郡山|廿日市|下松|岩国|田川|大村)市|.+?郡(?:玉村|大町|.+?)[町村]|.+?市.+?区|.+?[市区町村])(.+)"
+
+    result = re.match(pattern, content)
+    prefecture = ""
+    city = ""
+    if result:
+        prefecture = result.group(1)
+        city = result.group(2)
+    else:
+        # 都道府県のみ
+        pattern2 = "(...??[都道府県])"
+        result_prefecture = re.match(pattern2, content)
+
+        if result_prefecture:
+            prefecture = result_prefecture.group(1)
+
+    return prefecture, city
+
+
+def str_to_int(s):
+    """ 文字列を数値に変換して返す。変換できない場合は0を返す """
+
+    if not s.isdecimal():
+        try:
+            i = float(s)
+        except ValueError:
+            i = 0
+    else:
+        i = int(s)
+    return i
+
+
+# 物件
+def estate_price(e):
+    """金額取得処理"""
+    price_str = e.select_one(".dottable-value").getText()
+    price = estate_price_form_str(price_str)
+    return price
+
+
+def estate_price_form_str(price_str):
+    """ 金額の文字列から数値としての価格を取得する（処理が複雑なので分離） """
+
+    # 円より後ろは無視
+    if "円" in price_str:
+        price_str = price_str.split("円")[0] + "円"
+
+    price_str = price_str.replace("億円", "0000").replace("万円", "")
+    # 万以下は無視
+    price_str = re.sub("万.+円", "", price_str)
+    # 億以上で万円が付く場合の処理
+    if "億" in price_str:
+        res = price_str.split("億")
+        price_str = str(int(res[0]) * 10000 + int(res[1]))
+
+    # 数値のみを抽出する
+    price_str = re.sub(r"\D", "", price_str)
+    price = str_to_int(price_str)
+
+    return price
+
+
+def estate_shop(e):
+    """店舗情報を抽出 """
+    shop = e.select_one(".shopmore-title")
+    if shop:
+        shop = shop.getText().replace("\t", "").replace("\n", "").replace("\r", "")
+    else:
+        shop = ""
+
+    return shop
+
+
+def estate_place(e):
+    """"場所と駅等を含む部分を返す """
+    # ddが１つ余計にはいっている場合の対策
+    dt_name = e.select(".dottable-line > dl > dt")[2].getText()
+    if dt_name == "所在地":
+        place = e.select(".dottable-line > dl > dd")[2].getText()
+        t = e.select(".dottable-line > dl > dd")[3].getText()
+    else:
+        place = e.select(".dottable-line > dl > dd")[1].getText()
+        t = e.select(".dottable-line > dl > dd")[2].getText()
+
+    return t, place
+
+
+def estate_tag_route_station_work(t):
+    """部分HTMLから物件の路線を取得"""
+    if "「" in t and "」" in t:
+        res = re.match(r"(.+)「(.+)」(.+)$", t)
+        if res:
+            cnt = len(res.groups())
+            route = res.group(1) if cnt > 0 else ""
+            station = res.group(2) if cnt > 1 else ""
+            work = res.group(3) if cnt > 2 else ""
+    else:
+        route = ""
+        station = ""
+        work = ""
+    return route, station, work
+
+
+def estate_area_buildingarea_to_int(str):
+    """ 土地面積と建物面積を抽出し、数値として返す """
+
+    if "m2" in str:
+        str = str.split("m2")[0]
+    if "㎡" in str:
+        str = str.split("㎡")[0]
+    num = str_to_int(str)
+    return num
+
+
+def picup_estates(estate_elems):
+    """BSオブジェクトから物件情報を抽出"""
+    estates = []
+    i = 0
+    for e in estate_elems:
+        h = {}
+        h["id"] = ""
+        h["note"] = e.select_one(".property_unit-title").getText().replace("\n", "")
+
+        h["price"] = estate_price(e)
+
+        h["shop"] = estate_shop(e)
+
+        t, h["place"] = estate_place(e)
+        # 都道府県・市町村を取得
+        h["prefecture"], h["city"] = get_prefecture_city(h["place"])
+
+        # tを分割する
+        route, station, work = estate_tag_route_station_work(t)
+        h["station"] = station
+        h["route"] = route
+        h["work"] = work
+
+        d = e.select(".dottable-line > table.dottable-fix dd")
+        h["area"] = estate_area_buildingarea_to_int(d[0].getText())
+        h["buildingarea"] = estate_area_buildingarea_to_int(d[2].getText())
+        h["ldk"] = d[1].getText()
+        h["buildingyear"] = str(d[3].getText())
+        h["url"] = sumo_domain + e.select_one(".property_unit-title a")["href"]
+
+        h["created"] = NOWTIME
+
+        # ハッシュ値の生成
+        txt = str(h["price"]) + h["place"] + str(h["area"]) + str(h["buildingarea"])
+        h["id"] = create_hash(txt)
+
+        estates.append(h)
+
+    return estates
+
+
+# ハッシュ値の生成
+def create_hash(txt):
+    hash = hashlib.sha256(txt.encode("utf-8")).hexdigest()
+    return hash
+
+
+# pandasでCSV変換
+def to_csv(estates, filename):
+    df = pd.DataFrame(data=estates)
+    df.to_csv(filename, index=False)
+
+
+# メッセージの作成 htmlmail
+def create_message(from_addr, to_addr, subject, body, filename=""):
+    msg = MIMEMultipart()
+    msg["Subject"] = subject
+    msg["From"] = from_addr
+    msg["To"] = to_addr
+    msg.attach(MIMEText(body, "html"))
+    # 添付ファイル処理
+    if filename:
+        with open(filename, "rb") as f:
+            mb = MIMEApplication(f.read())
+        mb.add_header("Content-Disposition", "attachment", filename=filename)
+        msg.attach(mb)
+    return msg
+
+
+# メールの送信
+def send_mail(msg):
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login(my_addr, my_pass)
+        server.send_message(msg)
+
+
+def create_time_check(filename):
+    """ CSV取得から１時間経過していなかったらFalse """
+
+    # ファイルが存在しない場合はTrue
+    if os.path.isfile(filename) == False:
+        return True
+
+    p = Path(filename)
+    create_time = p.stat().st_ctime
+    now = time.time()
+
+    # 3600秒 3時間以上立っている場合はTrue
+    after_time = now - (create_time + (3600 * 3))
+
+    if after_time > 0:
+        return True
+    else:
+        return False
+
+
+def main(title, get_url):
+    print("### 処理開始 ###")
+    # 速度対策で、処理が５時間以内にされている場合は、CSV取得処理をスキップして処理続行
+    filename = "./tmp/_" + title + ".csv"
+    if create_time_check(filename):
+        estates = picup_estates(get_estate(get_url))
+        to_csv(estates, filename)
+
+    else:
+        print("### SKIP ###")
+
+    # 取得CSVからGSに登録、処理が失敗した場合はスキップして継続
+    try:
+        new_estates = get_google_spreadsheet.append_new_estate(filename)
+    except:
+        pass
+
+    # if estates:
+    #     body = ""
+    #     msg = create_message(my_addr, my_addr, "Estatesおしらせ", body, filename)
+    #     send_mail(msg)
+
+    print("### 処理完了 ###")
+
---
+++ tests/maintests.py
@@ -0,0 +1,180 @@
+""" メイン処理のテスト """
+import unittest as ut
+from estates import main
+
+from bs4 import BeautifulSoup
+
+
+class MainTest(ut.TestCase):
+    """Mainのテスト"""
+
+    def test_get_estate(self):
+        """ 不動産情報を取得する """
+
+        url_yokosuka = "https://suumo.jp/jj/bukken/ichiran/JJ012FC001/?ar=030&bs=021&sc=14201&ta=14&po=1&pj=2&pc=100"
+        estate_elems = main.get_estate(url_yokosuka)
+        self.assertNotEqual(estate_elems, False)
+
+    def test_create_hash(self):
+        """ ハッシュの生成を確認 """
+
+        txt = ""
+        hash = main.create_hash(txt)
+        self.assertEqual(
+            hash, "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+        )
+
+        txt = "29800hogehoge"
+        hash = main.create_hash(txt)
+        self.assertEqual(
+            hash, "73553c150387ac0b5bc61b6ede3f64b55c71ad5ae294f72cf735bcc037439e4d"
+        )
+
+    def test_estate_tag_route_station_work(self):
+        """ 駅などの情報から、駅名、路線名、所要時間を抽出 """
+
+        t = "妙高はねうまライン「高田」徒歩32分"
+        route, station, work = main.estate_tag_route_station_work(t)
+
+        self.assertEqual(route, "妙高はねうまライン")
+        self.assertEqual(station, "高田")
+        self.assertEqual(work, "徒歩32分")
+
+        t = ""
+        route, station, work = main.estate_tag_route_station_work(t)
+
+        self.assertEqual(route, "")
+        self.assertEqual(station, "")
+        self.assertEqual(work, "")
+
+        t = "-"
+        route, station, work = main.estate_tag_route_station_work(t)
+
+        self.assertEqual(route, "")
+        self.assertEqual(station, "")
+        self.assertEqual(work, "")
+
+        # 閉じカッコがないケース
+        t = "ほげほえ「あああああ"
+        route, station, work = main.estate_tag_route_station_work(t)
+
+        self.assertEqual(route, "")
+        self.assertEqual(station, "")
+        self.assertEqual(work, "")
+
+        # エラーになったケース　カッコ２つ
+        t = "富士急静岡バス「「高山団地入口」停より徒歩」徒歩4分"
+        route, station, work = main.estate_tag_route_station_work(t)
+
+        self.assertEqual(route, "富士急静岡バス「")
+        self.assertEqual(station, "高山団地入口」停より徒歩")
+        self.assertEqual(work, "徒歩4分")
+
+    def test_estate_price(self):
+        """ 価格取得処理のテスト """
+        filename = "tests/html/estates.html"
+        estates = main.html_to_estates(open(filename))
+        # 通常ケース
+        e = estates[0]
+        price = main.estate_price(e)
+        self.assertEqual(price, 2390)
+
+        # 文字列があるケース 文字列：　2580※権利金含む
+        e = estates[1]
+        price = main.estate_price(e)
+        self.assertEqual(price, 2580)
+
+    def test_estate_price_form_str(self):
+        """ いろいろな価格文字列のケースを想定 """
+
+        s = ""
+        price = main.estate_price_form_str(s)
+        self.assertEqual(price, 0)
+
+        s = "2356"
+        price = main.estate_price_form_str(s)
+        self.assertEqual(price, 2356)
+
+        # 円の場合は間違いと想定して、万円と同様の処理
+        s = "2356円"
+        price = main.estate_price_form_str(s)
+        self.assertEqual(price, 2356)
+
+        s = "2356万円"
+        price = main.estate_price_form_str(s)
+        self.assertEqual(price, 2356)
+
+        s = "2,356万円"
+        price = main.estate_price_form_str(s)
+        self.assertEqual(price, 2356)
+
+        s = "99億円"
+        price = main.estate_price_form_str(s)
+        self.assertEqual(price, 990000)
+
+        s = "99億2356万円"
+        price = main.estate_price_form_str(s)
+        self.assertEqual(price, 992356)
+
+        s = "ある2,356円"
+        price = main.estate_price_form_str(s)
+        self.assertEqual(price, 2356)
+
+        s = "ある2,356円～"
+        price = main.estate_price_form_str(s)
+        self.assertEqual(price, 2356)
+
+        s = "ある2,356円～2500円"
+        price = main.estate_price_form_str(s)
+        self.assertEqual(price, 2356)
+
+    def test_estate_area_buildingarea_to_int(self):
+        """ 面積を含む数字文字列から面積を数値で取得する """
+
+        s = "455.71m2（137.85坪）（登記）"
+        i = main.estate_area_buildingarea_to_int(s)
+        self.assertEqual(i, 455.71)
+
+        # m2が存在しない場合
+        s = "455.71m（137.85坪）（登記）"
+        i = main.estate_area_buildingarea_to_int(s)
+        self.assertEqual(i, 0)
+
+    def test_picup_estates(self):
+        """ 取得したHTMLから不動産情報を抽出 全体的なテストなので一番下"""
+
+        filename = "tests/html/yokosuka.html"
+        soup = main.html_to_estates(open(filename))
+        estates = main.picup_estates(soup)
+
+        self.assertEqual(
+            estates[0]["id"],
+            "ed09d1eb22360c8066feab3ed90555488d26b8e042e652602bf60d928f462a86",
+        )
+
+        self.assertEqual(estates[0]["price"], 1040)
+        self.assertEqual(estates[0]["place"], "神奈川県横須賀市小矢部１")
+        self.assertEqual(estates[0]["prefecture"], "神奈川県")
+        self.assertEqual(estates[0]["city"], "横須賀市")
+        self.assertEqual(estates[0]["station"], "衣笠")
+        self.assertEqual(estates[0]["route"], "ＪＲ横須賀線")
+        self.assertEqual(estates[0]["area"], 455.71)
+        self.assertEqual(estates[0]["buildingarea"], 61.48)
+
+        # 2021/5/18 上越エラー
+        filename = "tests/html/jouetu.html"
+        soup = main.html_to_estates(open(filename))
+        estates = main.picup_estates(soup)
+
+        self.assertEqual(
+            estates[0]["id"],
+            "e93510230944e66657881e5f7ead6c4b2f7852647da35e6678f1dc1b5d412cab",
+        )
+        self.assertEqual(estates[0]["price"], 150)
+        self.assertEqual(estates[0]["place"], "新潟県上越市大字戸野目856")
+        self.assertEqual(estates[0]["prefecture"], "新潟県")
+        self.assertEqual(estates[0]["city"], "上越市")
+        self.assertEqual(estates[0]["station"], "高田")
+        self.assertEqual(estates[0]["route"], "妙高はねうまライン")
+        self.assertEqual(estates[0]["area"], 386.86)
+        self.assertEqual(estates[0]["buildingarea"], 175.69)
