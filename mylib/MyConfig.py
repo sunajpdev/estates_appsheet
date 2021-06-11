811a05c5f6550b7843d1a89bf3e7561dc4aea611
suna <sunajp.dev@gmail.com>
2021-06-12 08:42:48 +0900

    U 開発環境と本番環境で使うconfig.iniを変更するようにした

--- mylib/MyConfig.py
+++ mylib/MyConfig.py
@@ -7,6 +7,16 @@ class MyConfig:
     def __init__(self):
         """ 環境設定を読み込み開発環境か本番環境かで読み込む設定ファイルを変更する """
 
+        env = configparser.ConfigParser()
+        env.read(".env", encoding="utf-8")
+
+        # 開発環境と本番環境で別々のconfig.iniを読み込む
         self.ini = configparser.ConfigParser()
-        self.ini.read("config.ini", encoding="utf-8")
+        if env["Default"]["mode"] == "Production":
+            config_filename = env["Production"]["config_filename"]
+        else:
+            config_filename = env["Development"]["config_filename"]
+
+        print(config_filename)
+        self.ini.read(config_filename, encoding="utf-8")
 
