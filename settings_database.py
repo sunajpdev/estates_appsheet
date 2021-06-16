75e0f30bf3c3c9c98501db15abaff7b402b85e79
suna <sunajp.dev@gmail.com>
2021-06-16 16:37:21 +0900

    bugfix dbの設定をconfigから読むように変更

--- settings_database.py
+++ settings_database.py
@@ -7,10 +7,18 @@ from sqlalchemy.ext.declarative import declarative_base
 
 import pandas as pd
 
+from settings import ReadIni as ri
+
+database = ri.ini.get("DB", "database")
+user = ri.ini.get("DB", "user")
+password = ri.ini.get("DB", "password")
+host = ri.ini.get("DB", "host")
+dbname = ri.ini.get("DB", "dbname")
+
+uri = f"{database}://{user}:{password}@{host}/{dbname}"
+
 Base = declarative_base()
-engine = create_engine(
-    "postgresql://postgres:hoge@localhost/estates_appsheet", echo=False
-)
+engine = create_engine(uri, echo=False)
 
 session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 Base.query = session.query_property()
