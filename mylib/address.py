70c51d95bde604dd1c94edd5f1148d589f3c9253
suna <sunajp.dev@gmail.com>
2021-06-16 19:10:27 +0900

    add 住所から都道府県を抽出 都道府県より前に文字列が入っていても都道府県市町村返す

--- mylib/address.py
+++ mylib/address.py
@@ -1,12 +1,36 @@
+from mylib import mytool
 import re
+from mylib.mytool import MyTool
 
 
 class Address:
+    @classmethod
+    def address_to_prefecture(cls, address):
+        """住所のみ抽出する"""
+        jsn = MyTool.json_file_to_dict("mylib/perfectures.json")
+
+        for prefecture in jsn["prefectures"]:
+            if prefecture["name"] in address:
+                return prefecture["name"]
+
+        return ""
+
+    @classmethod
+    def address_orthopaedy(cls, address):
+        """都道府県より前にある文字列を削除する"""
+        prefecture = cls.address_to_prefecture(address)
+        if prefecture:
+            res = address.split(prefecture)
+            address = prefecture + res[1] if len(res) > 1 else address
+        return address
+
     @classmethod
     def address_to_prefecture_and_city(cls, address):
         """住所から都道府県と市町村を抽出
         戻り値: 都道府県、市町村
         """
+
+        address = cls.address_orthopaedy(address)
 
         pattern = "(...??[都道府県])((?:旭川|伊達|石狩|盛岡|奥州|田村|南相馬|那須塩原|東村山|武蔵村山|羽村|十日町|上越|富山|野々市|大町|蒲郡|四日市|姫路|大和郡山|廿日市|下松|岩国|田川|大村)市|.+?郡(?:玉村|大町|.+?)[町村]|.+?市.+?区|.+?[市区町村])(.+)"
 
