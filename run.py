8a9ac84168bb71233d5f3d16bdd67278af5a58ad
suna <sunajp.dev@gmail.com>
2021-06-17 07:12:58 +0900

    U 不動産取得先URLをdata/cities_url.csvから取得するように変更

--- run.py
+++ run.py
@@ -1,16 +1,19 @@
-from estates import main
+import pandas as pd
 
+from estates import main
 import settings_city
 
-cities = settings_city.cities
 
+csv_filename = "data/cities_url.csv"
+df = pd.read_csv(csv_filename)
+rows = df.to_dict(orient="records")
+for row in rows:
+    csv_filename = "./tmp/" + row["prefecture_name"] + row["city_name"] + ".csv"
+    url = row["url"]
+    print("###", row["prefecture_name"], row["city_name"])
+    res = main.web_to_csv_and_db(url, csv_filename)
+    print("Insert: ", res)
 
-for prefecture in cities:
-    for city, url in prefecture.items():
-        print("###", city)
-        csv_filename = f"./tmp/_{city}.csv"
-        res = main.web_to_csv_and_db(url, csv_filename)
-        print("Insert: ", res)
 # GoogleSpreadSheet更新処理
 print("### Sheet update start ###")
 csv_filename = "tmp/upload.csv"
