88b346e1cffca11e44fa4bc3fdcd4b9ab856f449
suna <sunajp.dev@gmail.com>
2021-06-16 19:10:42 +0900

    テスト追加

--- tests/Mydb_test.py
+++ tests/Mydb_test.py
@@ -71,6 +71,55 @@ class MydbTest(ut.TestCase):
         res = self.mydb.insert_estate(row)
         self.assertEqual(False, res)
 
+        # 失敗しているケース
+        row = {
+            "id": "48e077ba80be0a9fa1760d8c4feeba4e7b027552c8dec6a17bf991d9e61393f4",
+            "note": "成育３（野江駅） 3480万円",
+            "price": "3480",
+            "shop": "(株)福屋不動産販売京橋店",
+            "place": "大阪府大阪市城東区成育３",
+            "prefecture": "大阪府",
+            "city": "大阪市城東区",
+            "station": "野江",
+            "route": "京阪本線",
+            "work": "徒歩3分",
+            "area": "75.29",
+            "buildingarea": "108.31",
+            "buildingyear": "2005年8月",
+            "ldk": "4LDK",
+            "url": "https://suumo.jp//chukoikkodate/osaka/sc_osakashijoto/nc_96055204/",
+            "created": "2021/6/15",
+        }
+        id = row["id"]
+        self.mydb.delete_estate(id)
+        res = self.mydb.insert_estate(row)
+        estate = session.query(Estate).filter(Estate.id == id).first()
+        self.assertEqual(estate.id, res)
+
+        row = {
+            "id": "858fe6ff8e43934764a382a994c75f584f00606301a91dd6ca872de6d53f85d5",
+            "note": "◆京王線で新宿方向へ、JR南武線で川崎・横浜方向へ\u30002路線利用可能な通勤・…",
+            "price": 1800,
+            "shop": "アルカス住宅販売(株)",
+            "place": "神奈川県川崎市多摩区菅馬場２",
+            "prefecture": "神奈川県",
+            "city": "川崎市多摩区",
+            "station": "稲田堤",
+            "route": "ＪＲ南武線",
+            "work": "徒歩13分",
+            "area": 49.64,
+            "buildingarea": 72.09,
+            "buildingyear": "1991年9月",
+            "ldk": "4LDK",
+            "url": "https://suumo.jp//chukoikkodate/kanagawa/sc_kawasakishitama/nc_95659875/",
+            "created": "2021/6/10",
+        }
+        id = row["id"]
+        self.mydb.delete_estate(id)
+        res = self.mydb.insert_estate(row)
+        estate = session.query(Estate).filter(Estate.id == id).first()
+        self.assertEqual(estate.id, res)
+
     def test_all_estate_to_csv(self):
         """すべてのestateをCSVに保存"""
         filename = "./tmp/all_estate.csv"
--- tests/address_test.py
+++ tests/address_test.py
@@ -29,5 +29,73 @@ class AddressTest(ut.TestCase):
         # 無効な住所
         address = "ほげほげ東京都八王子市下柚木３"
         prefecture, city = Address.address_to_prefecture_and_city(address)
-        self.assertEqual("", prefecture)
-        self.assertEqual("", city)
+        self.assertEqual("東京都", prefecture)
+        self.assertEqual("八王子市", city)
+
+        # 日本、〒292-0051 千葉県木更津市清川１丁目１９−２  2021/6/15
+        address = "日本、〒292-0051 千葉県木更津市清川１丁目１９−２"
+        prefecture, city = Address.address_to_prefecture_and_city(address)
+        self.assertEqual("千葉県", prefecture)
+        self.assertEqual("木更津市", city)
+
+        # 北海道
+        address = "〒060-0808 北海道札幌市北区北８条西５丁目"
+        prefecture, city = Address.address_to_prefecture_and_city(address)
+        self.assertEqual("北海道", prefecture)
+        self.assertEqual("札幌市北区", city)
+
+        # 京都府
+        address = "〒606-8501 京都府京都市左京区吉田本町"
+        prefecture, city = Address.address_to_prefecture_and_city(address)
+        self.assertEqual("京都府", prefecture)
+        self.assertEqual("京都市左京区", city)
+
+        # 神奈川県
+        address = "〒221-8686 神奈川県横浜市神奈川区六角橋３丁目２７−１"
+        prefecture, city = Address.address_to_prefecture_and_city(address)
+        self.assertEqual("神奈川県", prefecture)
+        self.assertEqual("横浜市神奈川区", city)
+
+    def test_address_to_prefecture(self):
+
+        # 前に余計な文字がある
+        address = "日本、〒292-0051 千葉県木更津市清川１丁目１９−２"
+        res = Address.address_to_prefecture(address)
+        self.assertEqual("千葉県", res)
+
+        # 前に余計な文字がある
+        address = "ほげほげ東京都八王子市下柚木３"
+        res = Address.address_to_prefecture(address)
+        self.assertEqual("東京都", res)
+
+        # 無効な住所
+        address = "ほげほげ大阪"
+        res = Address.address_to_prefecture(address)
+        self.assertEqual("", res)
+
+        # 東京都
+        address = "東京都八王子市下柚木３"
+        res = Address.address_to_prefecture(address)
+        self.assertEqual("東京都", res)
+
+    def test_address_orthopaedy(self):
+
+        # 前に余計な文字がある
+        address = "日本、〒292-0051 千葉県木更津市清川１丁目１９−２"
+        res = Address.address_orthopaedy(address)
+        self.assertEqual("千葉県木更津市清川１丁目１９−２", res)
+
+        # 前に余計な文字がある
+        address = "ほげほげ東京都八王子市下柚木３"
+        res = Address.address_orthopaedy(address)
+        self.assertEqual("東京都八王子市下柚木３", res)
+
+        # 無効な住所
+        address = "ほげほげ大阪"
+        res = Address.address_orthopaedy(address)
+        self.assertEqual("ほげほげ大阪", res)
+
+        # 東京都
+        address = "東京都八王子市下柚木３"
+        res = Address.address_orthopaedy(address)
+        self.assertEqual("東京都八王子市下柚木３", res)
