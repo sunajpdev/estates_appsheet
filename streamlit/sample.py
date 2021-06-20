658bc9488f3f6b1df023cce7594aafaaf0f63b07
suna <sunajp.dev@gmail.com>
2021-06-20 10:09:23 +0900

    del noteディレクトリ　プロジェクト外に移動

---
+++ streamlit/sample.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+import plotly.graph_objs as go
+
+# タイトル
+st.title("My Sample")
+
+# 文字列
+st.write("Good morning")
+
+# Table
+st.table(
+    pd.DataFrame({"first column": [1, 2, 3, 4], "second column": [10, 20, 30, 40]})
+)
+
+# markdown
+st.markdown("# Markdown documents")
+
+
+# グラフ
+animals = ["giraffes", "orangutans", "monkeys", "a", "b"]
+populations = [20, 14, 23, 80, 90]
+
+fig = go.Figure(data=[go.Bar(x=animals, y=populations)])
+
+fig.update_layout(
+    xaxis=dict(
+        tickangle=0, title_text="Animal", title_font={"size": 20}, title_standoff=25
+    ),
+    yaxis=dict(title_text="Populations", title_standoff=25),
+    title="Title",
+)
+
+st.plotly_chart(fig, use_container_width=True)
