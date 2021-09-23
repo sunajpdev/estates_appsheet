e4e9084058555128e9fed89cb8a6b9f7d2ecbbf1
suna <sunajp.dev@gmail.com>
2021-09-23 16:53:21 +0900

    add index

--- flask/hello.py
+++ flask/hello.py
@@ -5,6 +5,16 @@ app = Flask(__name__)
 from markupsafe import escape
 
 
+@app.route("/")
+def index():
+    return "Index Page"
+
+
+@app.route("/hello")
+def hello():
+    return "Hello, World"
+
+
 @app.route("/user/<username>")
 def show_user_profile(username):
     # show the user profile for that user
