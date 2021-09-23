a7740accbb5c1297251c3439fafdadcdb4b92cf6
suna <sunajp.dev@gmail.com>
2021-09-24 03:42:03 +0900

    add hello.py

--- flask/hello.py
+++ flask/hello.py
@@ -1,13 +1,8 @@
 from flask import Flask
-from flask import url_for
 
 app = Flask(__name__)
 
 
-from flask import render_template
-
-
-@app.route("/hello/")
-@app.route("/hello/<name>")
-def hello(name=None):
-    return render_template("hello/hello.html", name=name)
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
