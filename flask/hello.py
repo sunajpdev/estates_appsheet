e1d73ccef98d8ea0a324bb3a248d693dda388ee5
suna <sunajp.dev@gmail.com>
2021-09-23 17:04:14 +0900

    render_template

--- flask/hello.py
+++ flask/hello.py
@@ -4,23 +4,10 @@ from flask import url_for
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return "index"
+from flask import render_template
 
 
-@app.route("/login")
-def login():
-    return "login"
-
-
-@app.route("/user/<username>")
-def profile(username):
-    return f"{username}'s profile"
-
-
-with app.test_request_context():
-    print(url_for("index"))
-    print(url_for("login"))
-    print(url_for("login", next="/"))
-    print(url_for("profile", username="John Doe"))
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello(name=None):
+    return render_template("hello/hello.html", name=name)
