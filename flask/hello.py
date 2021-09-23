5ce5fe2662f768655a57a34b14ef7f46c62ef5aa
suna <sunajp.dev@gmail.com>
2021-09-23 16:48:44 +0900

    routing

--- flask/hello.py
+++ flask/hello.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 from markupsafe import escape
 
 
-@app.route("/<name>")
-def hello(name):
-    return f"Hello, {escape(name)}!"
+@app.route("/")
+def index():
+    return "Index Page"
+
+
+@app.route("/hello")
+def hello():
+    return "Hello, World"
