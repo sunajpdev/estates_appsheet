0ba18005c2c49ebb9da685ba98d4cbcdaff54205
suna <sunajp.dev@gmail.com>
2021-09-23 16:52:16 +0900

    Unique URLs / Redirection Behavior

--- flask/hello.py
+++ flask/hello.py
@@ -21,3 +21,13 @@ def show_post(post_id):
 def show_subpath(subpath):
     # show the subpath after /path/
     return f"Subpath {escape(subpath)}"
+
+
+@app.route("/projects/")
+def projects():
+    return "The project page"
+
+
+@app.route("/about")
+def about():
+    return "The about page"
